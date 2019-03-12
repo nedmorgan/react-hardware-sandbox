25234394e01cbf702e3d74edfa213328cce02f77
nedmorgan <nmorgan1987@gmail.com>
2019-03-12 12:19:04 -0400

    able to update showing fields with button

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 
 class HomePage extends Component {
+  state = {
+    itemCurrentlyOnSale: 'A Hammer',
+    editSaleItem: true
+  }
+
+  toggleEditSaleItem = () => {
+    // This variale needs to be names the same as the key above
+    const editSaleItem = !this.state.editSaleItem
+    // this.setState({editSaleItem: editSaleItem})
+    this.setState({ editSaleItem })
+  }
   render() {
     return (
-      <div>Home Page</div>
+      <div>
+        <h1>My Hardware Store</h1>
+        <div><span>Currently on Sale: {this.state.itemCurrentlyOnSale}!</span>
+          <span><button onClick={this.toggleEditSaleItem}>{this.state.editSaleItem ? 'Hide' : 'Edit Sale Item'}</button></span>
+          {this.state.editSaleItem ? <div><input type="text" value={this.state.itemCurrentlyOnSale} /></div> : null}
+        </div>
+      </div>
     )
   }
 }
